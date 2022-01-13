da1b98910cbefecc2ebfc0c037010090e75d5295
siweiqi <siweiqi@ivymobile.com>
2022-01-13 10:27:43 +0800

    a

--- src/main/java/com/roman/garden/adeasy/AdInfo.java
+++ src/main/java/com/roman/garden/adeasy/AdInfo.java
@@ -2,33 +2,18 @@ package com.roman.garden.adeasy;
 
 public final class AdInfo {
 
+    //group
     public static final String GROUP_ADMOB = "admob";
     public static final String GROUP_UNITY = "unity";
     public static final String GROUP_VUNGLE = "vungle";
     @Deprecated
     public static final String GROUP_PANGLE = "pangle";
     public static final String GROUP_ADCOLONY = "adcolony";
-
+    //type
     public static final String TYPE_BANNER = "banner";
     public static final String TYPE_INTERSTITIAL = "interstitial";
     public static final String TYPE_VIDEO = "video";
     public static final String TYPE_NATIVE = "native";
-
-    public static final String EVENT_AD_LOADED = "adAdLoaded";
-    public static final String EVENT_AD_LOAD_FAILED = "onAdLoadFailed";
-    public static final String EVENT_AD_CLOSED = "onAdClosed";
-    public static final String EVENT_AD_OPENED = "onAdOpened";
-    public static final String EVENT_AD_IMPRESSION = "onAdImpression";
-    public static final String EVENT_AD_CLICKED = "onAdClicked";
-    public static final String EVENT_AD_SHOW_FAILED = "onAdShowFailed";
-
-    public static String makeMsg(String event, String group, String adType) {
-        return "{" + event + ":" + group + ", " + adType + "}";
-    }
-
-    public static String makeExtraMsg(String event, String group, String adType, String extraTitle, String extra) {
-        return "{" + event + ":" + group + ", " + adType + ", " + extraTitle + ":" + extra + "}";
-    }
 
 
 }
