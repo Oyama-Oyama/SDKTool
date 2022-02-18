13f67f732bcd38cba8eef82c3a463154c98f8af2
siweiqi <siweiqi@ivymobile.com>
2022-02-18 17:02:45 +0800

    a

--- SDKTool/src/main/java/com/roman/garden/adeasy/util/StringUtil.java
+++ SDKTool/src/main/java/com/roman/garden/adeasy/util/StringUtil.java
@@ -10,5 +10,20 @@ public class StringUtil {
         return false;
     }
 
+    public static boolean equal(String arg1, String arg2) {
+        if (isEmpty(arg1) || isEmpty(arg2))
+            return false;
+        if (arg1.equals(arg2))
+            return true;
+        return false;
+    }
+
+    public static boolean equalIgnoreCase(String arg1, String arg2) {
+        if (isEmpty(arg1) || isEmpty(arg2))
+            return false;
+        if (arg1.equalsIgnoreCase(arg2))
+            return true;
+        return false;
+    }
 
 }
